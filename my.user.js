e5d54c6d6f89078fc021d58d484099ca7e844b8c
MUTED64 <muted32555664@gmail.com>
2022-11-07 22:54:21 +0800

    Feature: Interactive danmaku style settings in control bar

--- my.user.js
+++ my.user.js
@@ -2,13 +2,14 @@
 // ==UserScript==
 // @name         SakuraDanmaku
 // @namespace    https://muted.top/
-// @version      0.2
+// @version      0.3
 // @description  yhdm, but with Danmaku from Bilibili
 // @author       MUTED64
 // @match        https://www.yhdmp.cc/yxsf/player/dpx2/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yhdmp.cc
 // @grant        GM_xmlhttpRequest
 // @require      https://cdn.jsdelivr.net/npm/danmaku@latest/dist/danmaku.min.js
+// @run-at       document-end
 // ==/UserScript==
 
 "use strict";
@@ -52,10 +53,6 @@ class BilibiliDanmaku {
         if (!mode) return null;
         const fontSize = Number(values[2]) || 25;
         const color = `000000${Number(values[3]).toString(16)}`.slice(-6);
-        // const gray =
-        //   ("0x"+color.slice(0, 2) | 0) * 0.299 +
-        //   ("0x"+color.slice(2, 4) | 0) * 0.587 +
-        //   ("0x"+color.slice(4, 6) | 0) * 0.114;
         return {
           text: $d.childNodes[0].nodeValue,
           mode,
@@ -63,7 +60,7 @@ class BilibiliDanmaku {
           style: {
             fontSize: `${fontSize}px`,
             color: `#${color}`,
-            textShadow: "0px 1px 4px #000a",
+            textShadow: "0px 1px 3px #000,0px 0px 3px #000",
             font: `${fontSize}px sans-serif`,
             fillStyle: `#${color}`,
             strokeStyle: color === "000000" ? "#fff" : "#000",
@@ -143,7 +140,206 @@ class DanmakuLoader {
   }
 }
 
+class DanmakuSettings {
+  danmakuWrapper;
+  danmakuButton;
+  danmakuSettingBox;
+  showOrHideDanmaku;
+  buttonHtml =
+    "<button class=\"dplayer-icon dplayer-comment-icon\" data-balloon=\"弹幕设置\" data-balloon-pos=\"up\"><span class=\"dplayer-icon-content\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 32 32\"><path d=\"M27.128 0.38h-22.553c-2.336 0-4.229 1.825-4.229 4.076v16.273c0 2.251 1.893 4.076 4.229 4.076h4.229v-2.685h8.403l-8.784 8.072 1.566 1.44 7.429-6.827h9.71c2.335 0 4.229-1.825 4.229-4.076v-16.273c0-2.252-1.894-4.076-4.229-4.076zM28.538 19.403c0 1.5-1.262 2.717-2.819 2.717h-8.36l-0.076-0.070-0.076 0.070h-11.223c-1.557 0-2.819-1.217-2.819-2.717v-13.589c0-1.501 1.262-2.718 2.819-2.718h19.734c1.557 0 2.819-0.141 2.819 1.359v14.947zM9.206 10.557c-1.222 0-2.215 0.911-2.215 2.036s0.992 2.035 2.215 2.035c1.224 0 2.216-0.911 2.216-2.035s-0.992-2.036-2.216-2.036zM22.496 10.557c-1.224 0-2.215 0.911-2.215 2.036s0.991 2.035 2.215 2.035c1.224 0 2.215-0.911 2.215-2.035s-0.991-2.036-2.215-2.036zM15.852 10.557c-1.224 0-2.215 0.911-2.215 2.036s0.991 2.035 2.215 2.035c1.222 0 2.215-0.911 2.215-2.035s-0.992-2.036-2.215-2.036z\"></path></svg></span></button>";
+
+  constructor(danmaku, danmakuDOM) {
+    this.danmaku = danmaku;
+    this.danmakuDOM = danmakuDOM;
+    this.#createButton();
+    this.#createSettingBox();
+    this.#addSettingItems();
+  }
+
+  #createButton() {
+    // 创建按钮
+    this.danmakuWrapper = document.createElement("div");
+    this.danmakuWrapper.setAttribute("class", "dplayer-setting");
+    this.danmakuWrapper.setAttribute(
+      "style",
+      "display:inline-block;height:100%;"
+    );
+    this.danmakuWrapper.innerHTML = this.buttonHtml;
+    // 添加到播放器
+    const playerIconsRight = document.querySelector(
+      "div.dplayer-controller > div.dplayer-icons.dplayer-icons-right"
+    );
+    playerIconsRight.prepend(this.danmakuWrapper);
+    this.danmakuButton = this.danmakuWrapper.firstChild;
+
+    // 设定打开和关闭面板的操作
+    this.danmakuButton.addEventListener("click", () => {
+      if (this.danmakuSettingBox.style.transform === "scale(0)") {
+        this.danmakuSettingBox.style.transform = "scale(1)";
+      } else {
+        this.danmakuSettingBox.style.transform = "scale(0)";
+      }
+    });
+    document.addEventListener("click", (e) => {
+      if (
+        !e.path.includes(this.danmakuButton) &&
+        !e.path.includes(this.danmakuSettingBox)
+      ) {
+        this.danmakuSettingBox.style.transform = "scale(0)";
+      }
+    });
+  }
+
+  #createSettingBox() {
+    this.danmakuSettingBox = document.createElement("div");
+    this.danmakuSettingBox.setAttribute("class", "dplayer-setting-box");
+    this.danmakuSettingBox.setAttribute("style", "transform: scale(0);");
+    this.danmakuSettingBox.setAttribute("style", "width: 200px;");
+    this.danmakuWrapper.appendChild(this.danmakuSettingBox);
+  }
+
+  #addSettingItems() {
+    this.#addShowOrHide();
+    this.#addDanmakuSpeed();
+    this.#addDanmakuOpacity();
+    this.#addDanmakuFontSize();
+    this.#addDanmakuLimit();
+  }
+
+  #addShowOrHide() {
+    this.showOrHideDanmaku = document.createElement("div");
+    this.showOrHideDanmaku.setAttribute(
+      "class",
+      "dplayer-setting-item show-danmaku"
+    );
+    this.showOrHideDanmaku.style.display = "block";
+    this.showOrHideDanmaku.innerHTML =
+      "<span class=\"dplayer-label\">显示弹幕</span>";
+    this.showOrHideToggle = document.createElement("input");
+    this.showOrHideToggle.setAttribute("type", "checkbox");
+    this.showOrHideToggle.setAttribute("checked", "checked");
+    this.showOrHideToggle.style.float = "right";
+    this.showOrHideToggle.addEventListener("click", () => {
+      if (this.showOrHideToggle.checked) {
+        danmakuDOM.setAttribute("style", "display:block;");
+      } else {
+        danmakuDOM.setAttribute("style", "display:none;");
+      }
+    });
+    this.showOrHideDanmaku.appendChild(this.showOrHideToggle);
+    this.danmakuSettingBox.appendChild(this.showOrHideDanmaku);
+  }
+
+  #addDanmakuSpeed() {
+    this.danmakuSpeed = document.createElement("div");
+    this.danmakuSpeed.setAttribute(
+      "class",
+      "dplayer-setting-item speed-danmaku"
+    );
+    this.danmakuSpeed.style.display = "block";
+    this.danmakuSpeed.innerHTML = "<span class=\"dplayer-label\">弹幕速度</span>";
+    this.danmakuSpeedRange = document.createElement("input");
+    this.danmakuSpeedRange.setAttribute("type", "range");
+    this.danmakuSpeedRange.style.display = "inline-block";
+    this.danmakuSpeedRange.style.float = "right";
+    this.danmakuSpeedRange.style.width = "50%";
+    this.danmakuSpeedRange.setAttribute("min", 72);
+    this.danmakuSpeedRange.setAttribute("max", 288);
+    this.danmakuSpeedRange.setAttribute("value", 144);
+    this.danmakuSpeedRange.addEventListener("input", () => {
+      this.danmaku.speed = Number(this.danmakuSpeedRange.value);
+    });
+    this.danmakuSpeed.appendChild(this.danmakuSpeedRange);
+    this.danmakuSettingBox.appendChild(this.danmakuSpeed);
+  }
+
+  #addDanmakuOpacity() {
+    this.danmakuOpacity = document.createElement("div");
+    this.danmakuOpacity.setAttribute(
+      "class",
+      "dplayer-setting-item transparency-danmaku"
+    );
+    this.danmakuOpacity.style.display = "block";
+    this.danmakuOpacity.innerHTML =
+      "<span class=\"dplayer-label\">弹幕透明度</span>";
+    this.danmakuOpacityRange = document.createElement("input");
+    this.danmakuOpacityRange.setAttribute("type", "range");
+    this.danmakuOpacityRange.style.display = "inline-block";
+    this.danmakuOpacityRange.style.float = "right";
+    this.danmakuOpacityRange.style.width = "50%";
+    this.danmakuOpacityRange.setAttribute("min", 0);
+    this.danmakuOpacityRange.setAttribute("max", 1);
+    this.danmakuOpacityRange.setAttribute("step", 0.1);
+    this.danmakuOpacityRange.setAttribute("value", 1);
+    this.danmakuOpacityRange.addEventListener("input", () => {
+      danmakuDOM.style.opacity = Number(this.danmakuOpacityRange.value);
+    });
+    this.danmakuOpacity.appendChild(this.danmakuOpacityRange);
+    this.danmakuSettingBox.appendChild(this.danmakuOpacity);
+  }
+
+  #addDanmakuFontSize() {
+    this.danmakuFontSize = document.createElement("div");
+    this.danmakuFontSize.setAttribute(
+      "class",
+      "dplayer-setting-item font-size-danmaku"
+    );
+    this.danmakuFontSize.style.display = "block";
+    this.danmakuFontSize.innerHTML =
+      "<span class=\"dplayer-label\">弹幕字体大小</span>";
+    this.danmakuFontSizeRange = document.createElement("input");
+    this.danmakuFontSizeRange.setAttribute("type", "range");
+    this.danmakuFontSizeRange.style.display = "inline-block";
+    this.danmakuFontSizeRange.style.float = "right";
+    this.danmakuFontSizeRange.style.width = "50%";
+    this.danmakuFontSizeRange.setAttribute("min", 16);
+    this.danmakuFontSizeRange.setAttribute("max", 32);
+    this.danmakuFontSizeRange.setAttribute("step", 1);
+    this.danmakuFontSizeRange.setAttribute("value", 25);
+    this.danmakuFontSizeRange.addEventListener("input", () => {
+      for (const i of this.danmaku.comments) {
+        i.style.font = `${Number(
+          this.danmakuFontSizeRange.value
+        )}px sans-serif`;
+      }
+    });
+    this.danmakuFontSize.appendChild(this.danmakuFontSizeRange);
+    this.danmakuSettingBox.appendChild(this.danmakuFontSize);
+  }
+
+  #addDanmakuLimit() {
+    this.danmakuLimit = document.createElement("div");
+    this.danmakuLimit.setAttribute(
+      "class",
+      "dplayer-setting-item limit-danmaku"
+    );
+    this.danmakuLimit.style.display = "block";
+    this.danmakuLimit.innerHTML = "<span class=\"dplayer-label\">弹幕密度</span>";
+    this.danmakuLimitRange = document.createElement("input");
+    this.danmakuLimitRange.setAttribute("type", "range");
+    this.danmakuLimitRange.style.display = "inline-block";
+    this.danmakuLimitRange.style.float = "right";
+    this.danmakuLimitRange.style.width = "50%";
+    this.danmakuLimitRange.setAttribute("min", 0);
+    this.danmakuLimitRange.setAttribute("max", 1);
+    this.danmakuLimitRange.setAttribute("step", 0.01);
+    this.danmakuLimitRange.setAttribute("value", 1);
+    this.danmakuLimitRange.addEventListener("input", () => {
+      for (const i of this.danmaku.comments) {
+        i.style.display = "block";
+        if(Math.random()>Number(this.danmakuLimitRange.value)){
+          i.style.display = "none";
+        }
+      }
+    });
+    this.danmakuLimit.appendChild(this.danmakuLimitRange);
+    this.danmakuSettingBox.appendChild(this.danmakuLimit);
+  }
+}
+
 const episode_url = "https://www.bilibili.com/bangumi/play/ep693247";
-const episode = 1;
+const episode = 2;
 const danmakuLoader = new DanmakuLoader(episode_url, episode);
 await danmakuLoader.showDanmaku();
+const danmakuDOM = danmakuLoader.container.lastElementChild;
+new DanmakuSettings(danmakuLoader.danmaku);
